2c40778baf08c1c3f2a2bb3145757b8de0e01a0e
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-11-28 18:43:59 +0100

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 def remove_char_at(str, n):
     newstr = ""
     for x, y in enumerate(str):
-        if i !=n:
-            newstr += c
+        if x != n:
+            newstr += y
     return newstr
