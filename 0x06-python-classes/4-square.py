d7eccf31bffe1bd8a622b11dd4605e2fdc3c4041
SaliheddineAbdellah <abdellah.saliheddine.101@gmail.com>
2023-12-19 22:31:16 +0100

    classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines the square."""
+
+    def __init__(self, size=0):
+        """constructor.
+
+        Args:
+            size: len of a square side.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the len of a side of the square.
+
+        Raises
+            TypeError: if size not int
+            ValueError: if size <= 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Area of this square.
+
+        Returns:
+            The size squared.
+        """
+        return self.__size ** 2
